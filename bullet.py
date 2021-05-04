7e40a022969ad612d0ad1f3849ee0cde823de035
ymdm <ymdm9090@163.com>
2021-05-04 18:08:53 +0800

    修正bullet没有继承sprite类

--- bullet.py
+++ bullet.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Bullet:
+class Bullet(Sprite):
     """对飞船发射的子弹进行管理的类"""
     def __init__(self, ai_settings, screen, ship):
         super(Bullet, self).__init__()
--- game_functions.py
+++ game_functions.py
@@ -1,8 +1,9 @@
 import pygame
 import sys
+from bullet import Bullet
 
 
-def check_keydown_event(event, ship):
+def check_keydown_event(event, ai_settings, screen, ship, bullets):
     """响应按键"""
     if event.key == pygame.K_RIGHT:
         # 向右移动飞船
@@ -10,6 +11,10 @@ def check_keydown_event(event, ship):
     elif event.key == pygame.K_LEFT:
         # 向左移动飞船
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # 创建一颗子弹，并将其加入到编组bullets中
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_event(event, ship):
@@ -20,21 +25,23 @@ def check_keyup_event(event, ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """响应按键和鼠标事件函数"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # 鼠标单击退出按钮，退出游戏
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_event(event, ship)
+            check_keydown_event(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_event(event, ship)
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """刷新屏幕函数"""
     # 每次循环刷新屏幕
     screen.fill(ai_settings.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
     # 最近绘制的屏幕可见
     pygame.display.flip()
